c51cd6beedb56a2ae73e73f93d5bb06c9335d024
Fang Shu <colinshu1996@gmail.com>
2019-03-25 02:03:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,8 +2,10 @@ var playlist = {Ladygaga:"shallow"};
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist.artistName = songTitle;
+  return playlist
 }
 
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;
+  return playlist
 }
